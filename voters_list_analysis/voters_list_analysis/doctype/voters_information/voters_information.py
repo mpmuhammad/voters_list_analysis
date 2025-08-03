9ab456dcf76c1daf0681171dc3910919ccb22dec
mpmuhammad <muhammadmp77@gmail.com>
2025-08-03 12:06:29 +0530

    feat:doctypes

---
+++ voters_list_analysis/voters_list_analysis/doctype/voters_information/voters_information.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Muhammad and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class VotersInformation(Document):
+	pass
